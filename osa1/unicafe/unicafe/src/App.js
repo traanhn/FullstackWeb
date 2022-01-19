967e9744a432c3225ffdf3dda497c862eaccd4c4
traanhn <traanh92@gmail.com>
2022-01-19 21:07:08 +0200

    Add public files

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -1,86 +1,25 @@
-import React, { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
 
-const Header = (props) =>{
-  return <h1>{props.name}</h1>
-}
-
-const Button = (props) => {
+function App() {
   return (
-    <button onClick={props.handleClick}>
-      {props.name}
-    </button>
-  )
-}
-
-const StatisticsLine = (props) => {
-    return (
-        <tr>
-            <th> {props.text} </th>
-            <td> {props.value} </td>
-        </tr>
-    )
-
-}
-
-const Statistics = (props) => {
-  if (props.allClicks.length !== 0)
-    return (
-        <table>
-          <tbody>
-            <StatisticsLine text = "good" value = {props.good}/>
-            <StatisticsLine text = "neutral" value = {props.neutral}/>
-            <StatisticsLine text = "bad" value = {props.bad}/>
-            <StatisticsLine text = "total" value =  {props.good + props.bad + props.neutral}/>
-            <StatisticsLine text = "average" value =  {(props.good - props.bad)/(props.good  + props.neutral + props.bad)}/>
-            <StatisticsLine text = "positive" value =  {(props.good  + props.neutral)/(props.good  + props.neutral + props.bad)}/>
-          </tbody>
-        </table>
-
-  )
-  return (
-      <div>
-          No feedback given
-      </div>
-  )
-}
-
-
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const feedbackHeader = 'give feedback'
-  const statisticsHeader = 'statistics'
-  const list = ['good', 'neutral', 'bad']
-
-  const handleGoodClick = () => {
-    setAll(allClicks.concat('G'))
-    setGood(good + 1)
-  }
-
-  const handleNeutralClick = () => {
-    setAll(allClicks.concat('N'))
-    setNeutral(neutral + 1)
-  }
-
-   const handleBadClick = () => {
-    setAll(allClicks.concat('B'))
-    setBad(bad + 1)
-  }
-
-  return (
-    <div>
-      <Header name = {feedbackHeader}/>
-        <Button name = {list[0]} handleClick = {handleGoodClick}/>
-        <Button name = {list[1]} handleClick = {handleNeutralClick}/>
-        <Button name = {list[2]} handleClick = {handleBadClick}/>
-      <Header name = {statisticsHeader}/>
-      <Statistics good = {good} neutral = {neutral} bad={bad} allClicks = {allClicks}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
